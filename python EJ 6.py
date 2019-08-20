be2aedd0c66b92db0b343327709f76ad2cc54267
SvAndrey <54033917+SvAndrey@users.noreply.github.com>
2019-08-19 21:31:51 -0300

    Add files via upload

---
+++ python EJ 6.py
@@ -0,0 +1,17 @@
+def imprimeCositos(filas):
+    largo = filas*2
+    #parte 1
+    for i in range(filas):
+        for i in range(largo):
+            print("*", end="")
+        print("\n")
+    #parte 2
+    largo=2
+    for i in range(filas):
+        for i in range(largo):
+            print("*", end="")
+        largo += 2
+        print("\n")
+
+
+imprimeCositos(5)
